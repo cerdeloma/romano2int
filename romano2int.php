93a88db470a5c5fe9b308db8d45b829222e5b8af
marcelo cerdelo <marcelocerdelo@gmail.com>
2021-02-08 20:18:52 -0300

    brincando de transformar numeros romanos em inteiros

---
+++ romano2int.php
@@ -0,0 +1,37 @@
+<?php
+
+function romano2int($romano) {
+	$romano = strtoupper($romano);
+	$n = 0;
+	$numeros = [
+
+		'I' => 1,
+		'V' => 5,
+		'X' => 10,
+		'L' => 50,
+		'C' => 100,
+		'D' => 500,
+		'M' => 1000
+
+	];
+
+	$letras = strlen($romano);
+
+	for($i=0; $i<$letras; $i++){
+
+		$atual = $romano[$i];
+		$prox = (isset($romano[$i+1])) ? $romano[$i+1] : false;
+
+		if($prox && $numeros[$prox] > $numeros[$atual]){
+			$n -= $numeros[$atual];
+		}else{
+			$n += $numeros[$atual]; 
+		}
+
+	}
+
+	return $n;
+}
+
+$q = 'M';
+echo $q. ' = ' .romano2int($q);
